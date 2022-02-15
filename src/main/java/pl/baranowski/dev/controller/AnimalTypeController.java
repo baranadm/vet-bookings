bac64ee18ef8546bcdff350d07dc43cfed6953ae
baranadm <baranadm@gmail.com>
2022-02-15 21:10:13 +0100

    CORS to animalTypeController

--- src/main/java/pl/baranowski/dev/controller/AnimalTypeController.java
+++ src/main/java/pl/baranowski/dev/controller/AnimalTypeController.java
@@ -6,6 +6,7 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -21,6 +22,7 @@ import pl.baranowski.dev.exception.AnimalTypeAllreadyExistsException;
 import pl.baranowski.dev.exception.EmptyFieldException;
 import pl.baranowski.dev.service.AnimalTypeService;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/animalType")
 public class AnimalTypeController {
