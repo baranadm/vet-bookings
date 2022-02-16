6c61fade25d039fc11581e0fe7827e0568e83669
baranadm <baranadm@gmail.com>
2022-02-16 14:46:11 +0100

    cors...

--- src/main/java/pl/baranowski/dev/controller/VisitController.java
+++ src/main/java/pl/baranowski/dev/controller/VisitController.java
@@ -33,7 +33,7 @@ import pl.baranowski.dev.exception.VetNotActiveException;
 import pl.baranowski.dev.service.VetService;
 import pl.baranowski.dev.service.VisitService;
 
-@CrossOrigin(origins= {"https://baranadm.github.io/", "file:///home/daniel/Projects/vet-bookings-client/"})
+@CrossOrigin
 @RestController
 @RequestMapping("/visit")
 @Validated
