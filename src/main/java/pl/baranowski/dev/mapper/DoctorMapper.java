a6139178f53dc2ef9f880e887ee5c62bc94f2599
baranadm <baranadm@gmail.com>
2022-03-01 13:10:25 +0100

    added ignore mappings to DoctorMapper

--- src/main/java/pl/baranowski/dev/mapper/DoctorMapper.java
+++ src/main/java/pl/baranowski/dev/mapper/DoctorMapper.java
@@ -5,12 +5,16 @@ import org.mapstruct.factory.Mappers;
 import pl.baranowski.dev.dto.DoctorDTO;
 import pl.baranowski.dev.entity.Doctor;
 
-@Mapper(injectionStrategy = InjectionStrategy.CONSTRUCTOR)
+@Mapper(injectionStrategy = InjectionStrategy.CONSTRUCTOR, unmappedTargetPolicy = ReportingPolicy.ERROR)
 public interface DoctorMapper {
     DoctorMapper INSTANCE = Mappers.getMapper(DoctorMapper.class);
 
     DoctorDTO toDto(Doctor doctor);
 
     @InheritInverseConfiguration
+    @Mapping(target = "workingDays", ignore = true)
+    @Mapping(target = "worksFromHour", ignore = true)
+    @Mapping(target = "worksTillHour", ignore = true)
+    @Mapping(target = "visits", ignore = true)
     Doctor toEntity(DoctorDTO doctorDTO);
 }
