183f4380e7f4b6f6a678639c3c13057e5191c597
baranadm <baranadm@gmail.com>
2022-03-01 12:29:38 +0100

    added AnimalTypeMapper + tests

---
+++ src/main/java/pl/baranowski/dev/mapper/AnimalTypeMapper.java
@@ -0,0 +1,18 @@
+package pl.baranowski.dev.mapper;
+
+import org.mapstruct.InheritInverseConfiguration;
+import org.mapstruct.InjectionStrategy;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+import pl.baranowski.dev.dto.AnimalTypeDTO;
+import pl.baranowski.dev.entity.AnimalType;
+
+@Mapper(injectionStrategy = InjectionStrategy.CONSTRUCTOR)
+public interface AnimalTypeMapper {
+    AnimalTypeMapper INSTANCE = Mappers.getMapper(AnimalTypeMapper.class);
+
+    AnimalTypeDTO toDto(AnimalType animalType);
+
+    @InheritInverseConfiguration
+    AnimalType toEntity(AnimalTypeDTO animalTypeDTO);
+}
