778ee4076d6323c0e1d66ce6b9a83bbc75ce9dc0
baranadm <baranadm@gmail.com>
2022-03-01 12:57:11 +0100

    added ignored values in PatientMapper

--- src/main/java/pl/baranowski/dev/mapper/PatientMapper.java
+++ src/main/java/pl/baranowski/dev/mapper/PatientMapper.java
@@ -3,6 +3,7 @@ package pl.baranowski.dev.mapper;
 import org.mapstruct.InheritInverseConfiguration;
 import org.mapstruct.InjectionStrategy;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 import pl.baranowski.dev.dto.PatientDTO;
 import pl.baranowski.dev.entity.Patient;
@@ -14,5 +15,7 @@ public interface PatientMapper {
     PatientDTO toDto(Patient patient);
 
     @InheritInverseConfiguration
+    @Mapping(target = "withId", ignore = true)
+    @Mapping(target = "visits", ignore = true)
     Patient toEntity(PatientDTO patientDTO);
 }
