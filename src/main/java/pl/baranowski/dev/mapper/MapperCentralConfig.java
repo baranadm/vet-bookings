490c2fb12c618845a14533e32da0cb9140f15292
baranadm <baranadm@gmail.com>
2022-03-02 12:09:26 +0100

    Mapper config - componentModel to 'spring'

--- src/main/java/pl/baranowski/dev/mapper/MapperCentralConfig.java
+++ src/main/java/pl/baranowski/dev/mapper/MapperCentralConfig.java
@@ -4,7 +4,8 @@ import org.mapstruct.InjectionStrategy;
 import org.mapstruct.MapperConfig;
 import org.mapstruct.ReportingPolicy;
 
-@MapperConfig(injectionStrategy = InjectionStrategy.CONSTRUCTOR,
+@MapperConfig(componentModel = "spring",
+        injectionStrategy = InjectionStrategy.CONSTRUCTOR,
         unmappedTargetPolicy = ReportingPolicy.ERROR)
 public interface MapperCentralConfig {
 }
