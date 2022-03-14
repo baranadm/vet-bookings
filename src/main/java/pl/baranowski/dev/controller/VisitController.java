50a0efebdd113108dd2b759cb421875e6697fc21
baranadm <baranadm@gmail.com>
2022-03-14 12:23:38 +0100

    LOGGER

--- src/main/java/pl/baranowski/dev/controller/VisitController.java
+++ src/main/java/pl/baranowski/dev/controller/VisitController.java
@@ -1,44 +1,39 @@
 package pl.baranowski.dev.controller;
 
-import java.util.List;
-
-import javax.validation.Valid;
-import javax.validation.constraints.*;
-
-import org.springframework.beans.factory.annotation.Autowired;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.bind.annotation.*;
 import pl.baranowski.dev.dto.DoctorsFreeSlotsDTO;
 import pl.baranowski.dev.dto.NewVisitDTO;
 import pl.baranowski.dev.dto.VisitDTO;
 import pl.baranowski.dev.exception.InvalidParamException;
-import pl.baranowski.dev.exception.epoch.InvalidEpochTimeException;
 import pl.baranowski.dev.exception.NotFoundException;
+import pl.baranowski.dev.exception.epoch.InvalidEpochTimeException;
 import pl.baranowski.dev.service.VisitService;
+
+import javax.validation.Valid;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+import java.util.List;
 
 @CrossOrigin
 @RestController
 @RequestMapping("/visits")
 @Validated
 public class VisitController {
-
     public static final Pageable DEFAULT_PAGEABLE = PageRequest.of(0, 5);
-    @Autowired
-    VisitService visitService;
+    private static final Logger LOGGER = LoggerFactory.getLogger(VisitController.class);
+    private final VisitService visitService;
+
+    public VisitController(VisitService visitService) {
+        this.visitService = visitService;
+    }
 
     @GetMapping(value = "/check", produces = "application/json;charset=UTF-8")
     public @ResponseBody
@@ -47,10 +42,18 @@ public class VisitController {
             @RequestParam("medSpecialtyName") @NotBlank(message = "Invalid search criteria: medSpecialtyName should not be empty.") String medSpecialtyName,
             @RequestParam("epochStart") @NotBlank(message = "Invalid search criteria: epochStart should not be empty.") @Pattern(regexp = "[0-9]+", message = "Invalid epoch format - only digits allowed") String epochStart,
             @RequestParam("epochEnd") @NotBlank(message = "Invalid search criteria: epochEnd should not be empty.") @Pattern(regexp = "[0-9]+", message = "Invalid epoch format - only digits allowed") String epochEnd) throws InvalidEpochTimeException, NotFoundException, InvalidParamException {
+        LOGGER.debug(
+                "Received request: @GET '/visits/check', method: findFreeSlots(animalTypeName='{}', medSpecialtyName='{}', epochStart='{}', epochEnd='{}')",
+                animalTypeName,
+                medSpecialtyName,
+                epochStart,
+                epochEnd);
+
         List<DoctorsFreeSlotsDTO> result = visitService.findAvailableVisits(animalTypeName,
                                                                             medSpecialtyName,
                                                                             decodeEpoch(epochStart),
                                                                             decodeEpoch(epochEnd));
+        LOGGER.debug("Found {} free slots. Returning result.", result.size());
         return result;
     }
 
@@ -65,7 +68,11 @@ public class VisitController {
     @GetMapping(value = "/{id}", produces = "application/json;charset=UTF-8")
     public @ResponseBody
     VisitDTO getById(@Pattern(regexp = "\\d+", message = "Parameter [size] must be natural number.") @Min(1) @PathVariable String id) throws NumberFormatException, NotFoundException {
-        return visitService.getById(Long.decode(id));
+        LOGGER.debug("Received request: @GET '/visits/{id}', method: getById(id='{}')", id);
+
+        VisitDTO result = visitService.getById(Long.decode(id));
+        LOGGER.debug("Visit found: {}", result);
+        return result;
     }
 
     @GetMapping(value = "/", produces = "application/json;charset=UTF-8")
@@ -73,24 +80,33 @@ public class VisitController {
     Page<VisitDTO> findAll(
             @RequestParam("page") @Pattern(regexp = "\\d+", message = "Parameter [page] must be natural number, greater than or equal to 0.") String page,
             @RequestParam("size") @Pattern(regexp = "\\d+", message = "Parameter [size] must be natural number.") @Min(value = 1, message = "Parameter [size] must be greater than or equal to 1") String size) {
+        LOGGER.debug("Received request: @GET '/visits/', method: findAll(page='{}', size='{}')", page, size);
 
         Pageable pageable = PageRequest.of(Integer.parseInt(page), Integer.parseInt(size));
-        return visitService.findAll(pageable);
+        LOGGER.debug("Created pageable: {}", pageable);
+
+        Page<VisitDTO> result = visitService.findAll(pageable);
+        LOGGER.debug("Returning result: {}", result);
+        return result;
     }
 
     @PostMapping(value = "/", produces = "application/json;charset=UTF-8")
     @ResponseStatus(HttpStatus.CREATED)
-    // , binding result
-    // TODO dodać loggera
     public @ResponseBody
     VisitDTO addNew(@Valid @RequestBody NewVisitDTO nv) throws Exception {
+        LOGGER.debug("Received request: @POST '/visits/', method: addNew(NewVisitDTO): {}", nv);
 
         long doctorId = Long.decode(nv.getDoctorId());
         long patientId = Long.decode(nv.getPatientId());
         long epoch = Long.decode(nv.getEpoch());
+        LOGGER.debug("Creating new Visit for arguments: doctorId='{}', patientId='{}', epoch='{}'.",
+                     doctorId,
+                     patientId,
+                     epoch);
 
-        // TODO fasada/manager
-        return visitService.addNew(doctorId, patientId, epoch);
+        VisitDTO result = visitService.addNew(doctorId, patientId, epoch);
+        LOGGER.debug("Returning result: {}", result);
+        return result;
     }
 
 }
--- src/main/java/pl/baranowski/dev/service/VisitService.java
+++ src/main/java/pl/baranowski/dev/service/VisitService.java
@@ -47,31 +47,49 @@ public class VisitService {
     }
 
     public VisitDTO getById(long id) throws NotFoundException {
+        LOGGER.debug("getById(id='{}')", id);
         Visit result = findByIdOrThrow(id);
-        return visitMapper.toDto(result);
+        LOGGER.debug("Result: {}", result);
+
+        VisitDTO resultDTO = visitMapper.toDto(result);
+        LOGGER.debug("Returning DTO: {}", resultDTO);
+        return resultDTO;
     }
 
     private Visit findByIdOrThrow(long id) throws NotFoundException {
         return visitRepository.findById(id)
-                              .orElseThrow(() -> new NotFoundException("Visit with id: " + id + " has not been found"));
+                              .orElseThrow(() -> {
+                                  NotFoundException e = new NotFoundException("Visit with id: " + id + " has not been found");
+                                  LOGGER.debug(e.getMessage(), e);
+                                  return e;
+                              });
     }
 
     public Page<VisitDTO> findAll(Pageable pageable) {
+        LOGGER.debug("findAll(Pageable): {}", pageable);
         Page<Visit> result = visitRepository.findAll(pageable);
+        LOGGER.debug("Received Page of {} Visits.", result.getSize());
+
         Page<VisitDTO> pageOfDTOs = result.map(visitMapper::toDto);
+        LOGGER.debug("Returning Page of {} DTOs.", pageOfDTOs.getSize());
         return pageOfDTOs;
     }
 
     public VisitDTO addNew(Long doctorId,
                            Long patientId,
                            Long epochInSeconds) throws NewVisitNotPossibleException, NotFoundException, DoctorNotActiveException {
-        LOGGER.info("Received addNew() request with params: doctorId='{}', patientId='{}', epochInSeconds='{}'",
+        LOGGER.info("addNew(doctorId='{}', patientId='{}', epochInSeconds='{}')",
                     doctorId,
                     patientId,
                     epochInSeconds);
         Reception reception = new Reception(doctorService, patientService);
+        LOGGER.debug("Created new Reception: {}", reception);
+
         Visit possibleVisit = reception.createNewVisitIfPossible(doctorId, patientId, epochInSeconds);
+        LOGGER.debug("New Visit is possible: {}", possibleVisit);
+
         Visit savedVisit = visitRepository.save(possibleVisit);
+        LOGGER.debug("New Visit has been saved to database. Returning result: {}", savedVisit);
         return visitMapper.toDto(savedVisit);
     }
 
@@ -79,14 +97,23 @@ public class VisitService {
                                                          String medSpecialtyName,
                                                          Long epochStart,
                                                          Long epochEnd) throws InvalidEpochTimeException, NotFoundException {
+        LOGGER.debug("findAvailableVisits(animalTypeName='{}', medSpecialtyName='{}', epochStart='{}', epochEnd='{}')", animalTypeName, medSpecialtyName, epochStart, epochEnd);
+
         List<Doctor> matchingDoctors = findDoctors(animalTypeName, medSpecialtyName);
+        LOGGER.debug("Found {} matching doctors.", matchingDoctors.size());
 
         EpochFutureTimeRange timeRange = new EpochFutureTimeRange(epochStart, epochEnd);
+        LOGGER.debug("Created TimeRange: {}", timeRange);
+
         AvailableSlotsFinder slotsFinder = new AvailableSlotsFinder(matchingDoctors, timeRange);
+        LOGGER.debug("Created AvailableSlotsFinder: {}", slotsFinder);
 
         List<DoctorsFreeSlots> availableSlots = slotsFinder.find();
+        LOGGER.debug("Found {} doctors with free slots.", availableSlots.size());
+
         List<DoctorsFreeSlotsDTO> availableSlotsDTO = availableSlots.stream().map(slotsMapper::toDto).collect(
                 Collectors.toList());
+        LOGGER.debug("Mapping results to {} DTOs.", availableSlotsDTO.size());
         return availableSlotsDTO;
     }
 
@@ -105,5 +132,4 @@ public class VisitService {
                                                      String medSpecialtyName) throws NotFoundException {
         return doctorService.findByAnimalTypeNameAndMedSpecialtyName(animalTypeName, medSpecialtyName);
     }
-
 }
